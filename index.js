206fff78579202452951fb69f7f3773f24544db5
Juan Mrad <juansmrad@gmail.com>
2021-02-20 08:37:11 -0600

    remove models add comments for work

--- index.js
+++ index.js
@@ -7,44 +7,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/artist", function (req, response) {
-  console.log('querying artist');
-  models.artist.findAll().then(function (artists){
-    console.log(artists);
-    response.send(artists);
-  });
+  // add code to get all artists
 });
 
 app.put("/artist/:id", function (req, response) {
-  console.log('updating artist: ' + req.params.id);
-  let updateValues = {};
-
-  if(req.body.name) {
-    updateValues.name = req.body.name;
-  }
-
-  if(req.body.dob) {
-    updateValues.dob = req.body.dob;
-  }
-
-  console.log(updateValues);
-
-  models.artist.update(updateValues, { where: { id: req.params.id } })
-  .then(function (updated) {
-    console.log('updated success');
-    console.log(updated);
-    response.send(updated);
-  });
+  // add code to get specific artist
 });
 
 app.post("/artist", function (req, response) {
   console.log('creating artist');
-  console.log(req.body);
-  models.artist.create({name: req.body.name, dob: req.body.dob})
-  .then(function (artist){
-    console.log(artist);
-    response.send("new artist created with id: " + artist.id);
-  });
+  // add code to create artist
 });
+
+// Add code to get all albums. GET /album
+
+// Add code to get a specific albums. GET /album/:id
+
+// Add code to get a specific albums. GET /album/:id
+
+// Add code to create an album. POST /album
+
+// Add Code to get all songs from an album. GET /album/song
+
+// Add code to create a song for an album. POST /album/song
 
 app.listen(3000, function(){
   console.log('server listening on port 3000');
